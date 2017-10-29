eea7021078bf9ab94f78b88bcab7f3f57dc1fc42
Maksym <maksymilian.makuch7@gmail.com>
2017-10-29 11:43:35 +0100

    poprawione, rzeczywiście taby

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,18 @@ var countriesList = $('#countries');
 $('#search').click(searchCountries);
 
 function searchCountries() {
- 	var countryName = $('#country-name').val();
+	var countryName = $('#country-name').val();
 if(!countryName.length) countryName = 'Poland';
 	$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
 };
 
 function showCountriesList(resp) {
 	countriesList.empty();
 	resp.forEach(function(item){
-   		$('<li>').text(item.name).appendTo(countriesList);
+		$('<li>').text(item.name).appendTo(countriesList);
 	});
 }
